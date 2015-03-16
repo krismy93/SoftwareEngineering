48ceac378d65ab217cf1d79c5d4f1aef2be40904
krismy <krismy93@gmail.com>
2015-03-15 21:44:45 -0700

    added murder and robbery rate buttons

--- buttons.js
+++ buttons.js
@@ -1,16 +1,29 @@
-function changeRange() {
-   // programmaticSlider.setState({ 'lowValue': 1960, 'highValue': 1970 });
-   // programmaticSlider.draw();
-    var col = [1, 3];
-   // var collist = '{ columns: [1, 3] }';
-    programmaticChart.setColumns(col);
+/*
+*changeToRobbery()
+*
+*called when the Robbery Rate Button is pressed
+*Data for column chart now displays Robbery Rate for each state
+*
+*/
+function changeToRobbery() {
+    var columns = '{ "columns": [1, 3] }';
+    programmaticChart.setView(columns);
     programmaticChart.draw();
+    
 }
 
-function getChosenState() {
-    
 
+/*
+*changeToMurder()
+*
+*called when the Murder Rate Button is pressed
+*Data for column chart now displays Murder Rate for each state
+*
+*/
+function changeToMurder() {
+    var columns = '{ "columns": [1, 2] }';
+    programmaticChart.setView(columns);
 
-
+    programmaticChart.draw();
 
 }
